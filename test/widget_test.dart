4c42945a293a96ae6d83021e21e904fdbd7b6223
bastndev <gohit24@gmail.com>
2023-06-01 14:34:14 -0500

    :recycle: widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
-// This is a basic Flutter widget test.
+// This is a basic Flutter widget test. 
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
