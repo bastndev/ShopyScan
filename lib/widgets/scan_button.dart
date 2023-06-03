6ff037b2f6bd7b82763a25eaeb85edd007e898c9
bastndev <gohit24@gmail.com>
2023-06-03 10:49:15 -0500

    :arrow_up: QR Scan in Mobile

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -8,7 +9,10 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancel',false,ScanMode.QR);
+        print(barcodeScanRes);
+      },
     );
   }
 }
