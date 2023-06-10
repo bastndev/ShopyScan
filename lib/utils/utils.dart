ab99cf0914f97c4bfcbda9f3e8d824f2ab0becae
bastndev <gohit24@gmail.com>
2023-06-10 15:09:38 -0500

    :arrow_up: Qr Scan Model `v5.1.0`

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_qr/providers/db_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-launchURL( BuildContext context, ScanModel scan) async {
-
+launchURL(BuildContext context, ScanModel scan) async {
   final url = scan.valor;
-  
-  if (scan.type == 'http'){
+
+  if (scan.type == 'http') {
     // -- -- -- -- -- Open  web site
-    if(!await canLaunch((url),)){
-      await launch((url),);
-    }else{
-    throw 'Could not launch $url';
+    if (!await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
     }
-    
-  }else{
+  } else {
+    // ignore: avoid_print
     print('geo!!');
   }
 }
